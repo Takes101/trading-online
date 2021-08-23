0a63f6e9840558d96dc9fa11930b62e8254d9656
William Takes <william.e.takes@vanderbilt.edu>
2021-08-23 16:03:32 -0500

    category model

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -5,9 +5,19 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
-  },
+{
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  // define category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+  }
+},
   {
     sequelize,
     timestamps: false,
